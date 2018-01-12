f0af743ba8f9ada1020ec08405107cf68c3c6fca
Denys Oliveira <denysjau@gmail.com>
2018-01-12 01:07:50 -0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+yum update
+yum install nginx perl libaio perl-Data-Dumper.x86_64 -y
+groupadd mysql
+useradd -r -g mysql -s /bin/false mysql
+cd /usr/local
+wget https://dev.mysql.com/get/Downloads/MySQL-5.6/mysql-5.6.38-linux-glibc2.12-x86_64.tar.gz
+tar zxvf /usr/local/mysql-5.6.38-linux-glibc2.12-x86_64.tar.gz
+ln -s /usr/local/mysql-5.6.38-linux-glibc2.12-x86_64 mysql
+cd mysql
+scripts/mysql_install_db --user=mysql
+bin/mysqld_safe --user=mysql
+cp support-files/mysql.server /etc/init.d/mysqld
+/etc/init.d/mysqld start
+a=`cat arquivosbin | wc | cut -c6-7`
+cont=1
+let b="$a+1"
+         while [  $cont -lt $b ]; do
+                ln -s /usr/local/mysql/bin/`cat arquivosbin | sed -n "$cont"p` /bin/
+             let cont=cont+1 
+         done
+
+exit
+
